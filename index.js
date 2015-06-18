45940c821da1982d52e19832463faedd8a2d2ad6
rafaelmezzomo <rafaelmezzomo@gmail.com>
2015-06-18 16:22:23 -0300

    Update server definition

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
+var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000
+var port = 3000
+
 
 // app.get('/', function(req, res){
 //   res.sendFile(__dirname + '/index.html');
